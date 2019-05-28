cea11c9f1dfab8a36a93a5d0d6ebb941a01ac94d
Jsonni7 <51103679+Jsonni7@users.noreply.github.com>
2019-05-28 11:12:00 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import socket
+import machine
+import time
+import network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+  print('connecting to network...')
+  sta_if.active(True)
+  sta_if.connect('Yooooooo', 'wybzdmmssm')
+  while not sta_if.isconnected():
+    pass
+print('network config:', sta_if.ifconfig())
